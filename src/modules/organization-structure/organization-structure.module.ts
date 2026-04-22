932f8be4d338f4b6b77e711e2e1121300521aad4
Jean-Dv <jean.valencia01@proton.me>
2026-04-22 16:17:20 -0500

    feat: register organization-structure module routes with RouterModule

--- src/modules/organization-structure/organization-structure.module.ts
+++ src/modules/organization-structure/organization-structure.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { CompaniesModule } from './companies/companies.module';
 import { PositionsModule } from './positions/positions.module';
 import { WorkCentersModule } from './work-centers/work-centers.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [CompaniesModule, PositionsModule, WorkCentersModule],
+  imports: [
+    CompaniesModule,
+    PositionsModule,
+    WorkCentersModule,
+    RouterModule.register([
+      {
+        path: 'organization-structure',
+        children: [
+          { path: '', module: CompaniesModule },
+          { path: '', module: PositionsModule },
+          { path: '', module: WorkCentersModule },
+        ],
+      },
+    ]),
+  ],
 })
 export class OrganizationStructureModule {}
